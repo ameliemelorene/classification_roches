64ede28dac1fc644e6e85cafd96d12c056be576d
ameliemelorene <72452572+ameliemelorene@users.noreply.github.com>
2021-06-27 15:50:39 +0200

    tout ne marche pas vraiment mais un peu

---
+++ test_web.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return render_template('interface.html')
+
+@socketio.on("requete")
+def f(json):
+    ima=json["image"]
+    socketio.emit("reponse", {"premier_pixel" : 33}) 
+
+if __name__=="__main__":
+    socketio.run(app, port=5001)
